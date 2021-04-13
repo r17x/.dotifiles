d8a764e74c7402adf12e7c2d0eb7566ff67f9296
ri7nz <ri7nz@evilfactory.id>
2021-04-13 22:35:59 +0700

    feat(encrypt): add ssh

--- .config/zsh/Darwin.zsh
+++ .config/zsh/Darwin.zsh
@@ -1,3 +1,4 @@
 eval $(/opt/homebrew/bin/brew shellenv)
 test -e "${HOME}/.iterm2_shell_integration.zsh" \
     && source "${HOME}/.iterm2_shell_integration.zsh"
+export PATH="/opt/homebrew/opt/luajit-openresty/bin:$PATH"
