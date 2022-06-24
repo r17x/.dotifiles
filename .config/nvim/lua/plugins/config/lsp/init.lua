47b2ef120b1666c5ec87e76a2ce959fdd57fdead
r17x <ri7nz@evilfactory.id>
2022-06-24 20:17:38 +0700

    feat!(home, darwim): remove gpg related in darwin
    see: r17x/nixpkgs

--- .config/nvim/lua/plugins/config/lsp/init.lua
+++
@@ -1,6 +0,0 @@
-local setup = require 'plugins.config.lsp.setup'
-local capabilities = require 'plugins.config.lsp.capabilities'
-local attach = require 'plugins.config.lsp.attach'
-
-setup.handlers()
-setup.lsp(attach, capabilities)
